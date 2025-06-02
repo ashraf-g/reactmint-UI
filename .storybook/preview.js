fb4d32c1d057b3817097cab6b461672b29448e7a
Azreenmaulavi <azreenmaulavi29@gmail.com>
2025-06-02 14:57:25 +0530

    added moon icon for light/dark theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,7 +7,7 @@ export const globalTypes = {
     description: 'Global theme for components',
     defaultValue: 'light',
     toolbar: {
-      icon: 'circlehollow',
+      icon: 'moon',
       items: [
         { value: 'light', title: 'Light' },
         { value: 'dark', title: 'Dark' },
